ac49eecbba688d58fbe3c41aa2f5213f599fddec
yang-hsiang chang <leo424y@gmail.com>
2018-11-27 17:46:29 +0800

    state ok, search togo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,9 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.in_desc.all
+    # @q = Task.ransack(content_cont_any: params[:q][:content], state_cont_any: params[:q][:content])
+    # @tasks = @q.result.in_end_time_desc
+    @tasks = Task.in_end_time_desc
   end
 
   def new
@@ -36,7 +38,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:content, :end_time)
+    params.require(:task).permit(:content, :end_time, :state)
   end
 
   def set_task
