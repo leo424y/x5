2dc86e16c710f806e891fd088ef721946e3902f6
yang-hsiang chang <leo424y@gmail.com>
2018-11-27 13:19:18 +0800

    use have content to fix selector change after i18n

--- spec/features/user_add_task_spec.rb
+++ spec/features/user_add_task_spec.rb
@@ -4,27 +4,19 @@ RSpec.describe "Tasks", type: :feature do
   it "displays new task after successful create" do
     visit new_task_path
     fill_in "Content", with: "new task"
-    click_button "Create Task"
+    click_button "Create"
 
-    expect(page).to have_selector("td", text: "new task")
-  end
-
-  it "displays new task after unsuccessful create" do
-    visit new_task_path
-    fill_in "Content", with: "new task"
-    click_button "Create Task"
-
-    expect(page).to have_selector("td", text: "new task")
+    expect(page).to have_content(/new task/i)
   end
 
   it "displays edited task after successful edit" do
-    Task.create!(id: 1, content: 'new task')
+    Task.create!(id: 1, content: 'New Task')
 
     visit edit_task_path(1)
     fill_in "Content", with: "edited task"
-    click_button "Update Task"
+    click_button "Update"
 
-    expect(page).to have_selector("td", text: "edited task")
+    expect(page).to have_content(/edited Task/i)
   end 
 
   it "not displays edited task after successful destroy" do
@@ -33,6 +25,6 @@ RSpec.describe "Tasks", type: :feature do
     visit tasks_path
     click_link "Destroy"
 
-    expect(page).to_not have_selector("td", text: "new task")
+    expect(page).to_not have_content(/new task/i)
   end 
 end
