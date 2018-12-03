3dfdef07d4b99735f8212e8ca1cd58ce3fc1186b
yang-hsiang chang <leo424y@gmail.com>
2018-12-03 15:39:38 +0800

    i18n update

--- spec/features/09_i18n_info_spec.rb
+++ spec/features/09_i18n_info_spec.rb
@@ -10,8 +10,9 @@ RSpec.describe "Translate info to English through en.yml", type: :feature do
   it "displays English successful info after successful create" do
     visit new_task_path
     fill_in "Content", with: "new task"
+    select('todo', from: 'State')
     click_button "Create Task"
 
-    expect(page).to have_selector("p", text: "Task was successfully created!")
+    expect(page).to have_content(/Task was successfully created!/i)
   end
 end
