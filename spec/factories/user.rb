eb7774261915b66bc9aa1fecc1dd5a7be6517b5e
yang-hsiang chang <leo424y@gmail.com>
2018-12-03 09:12:10 +0800

    factorybot format update

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :user, class: User do
-    name 'Leo'
-    password 'y'
-    password_confirmation 'y'
-    email 'l@l'
-    role 'admin'
+    name { 'Leo' }
+    password { 'y' }
+    password_confirmation { 'y' }
+    email { 'l@l' }
+    role { 'admin' }
   end
 
   factory :user2, class: User do
-    name 'Fly'
-    password 'y'
-    password_confirmation 'y'
-    email 'ly@ly'
-    role 'normal'
+    name { 'Fly' }
+    password { 'y' }
+    password_confirmation { 'y' }
+    email { 'ly@ly' }
+    role { 'normal' }
   end
 end
